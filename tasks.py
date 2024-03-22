e0f0a7907c93a1fe74334b926d16982f5675d096
samushka13 <56408918+samushka13@users.noreply.github.com>
2024-03-22 03:25:41 +0200

    Added invoke tasks.

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py")
+
+@task
+def test(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage report -m")
+
+@task(test)
+def coverage_report_html(ctx):
+    ctx.run("coverage html")
+
+@task
+def lint(ctx):
+    ctx.run("pylint src")
